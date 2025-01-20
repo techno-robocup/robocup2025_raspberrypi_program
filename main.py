89bd68d0ccf1f3149b1fef5c5ca3a3a638ab7540
K10-K10 <keito.tottori@gmai.com>
2025-01-20 03:21:33 +0000

    Change Fn

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import time
 
 bus = smbus.SMBus(1) #Pass
 
-address = 0x08
+ADDRESS = 0x08
 
 def writeData():
     try:
         data = [0x01, 0x02, 0x03, 0x04]
-        bus.write_byte(address, data)
+        bus.write_byte(ADDRESS, data)
     except:
         return -1
 
 def readData():
     try:
-        data = bus.read_byte(address)
+        data = bus.read_byte(ADDRESS)
         return data
     except:
         return -1
