ab8d041da749ee5e0117e1e7b5fec40d4543c041
K10-K10 <keito.tottori@gmai.com>
2025-01-20 02:17:55 +0000

    add writeF

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ bus = smbus.SMBus(1) #Pass
 
 address = 0x08
 
+def writeData():
+    try:
+        data = [0x01, 0x02, 0x03, 0x04]
+        bus.write_byte(address, data)
+    except:
+        return -1
+
 def readData():
     try:
         data = bus.read_byte(address)
