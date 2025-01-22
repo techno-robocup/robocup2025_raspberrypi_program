d4e528f58419afa77f8b4fecf5c667c94b804d95
k10k100 <keito.tottori@gmail.com>
2025-01-22 15:51:39 +0900

    main.py

--- i2cio/i2cio.py
+++ i2cio/i2cio.py
@@ -7,7 +7,7 @@ class i2cio:
   def __init__(self, address):
     self.bus = smbus.SMBus(1)
     target_address = 0x08
-  
+
   def readData(self):
     try:
       data = self.bus.read_byte(address)
--- main.py
+++ main.py
@@ -4,25 +4,26 @@ import time
 bus = smbus.SMBus(1) #Pass
 
 ADDRESS = 0x08
-
 def writeData():
-    #byte_data = [ord(c) for c in data]
+    byte_data = [ord(c) for c in data]
     try:
-        bus.write_i2c_block_data(ADDRESS,)
+        bus.write_i2c_block_data(ADDRESS, ,0x00,byte_data)
     except:
         print("Error cannot write data")
         return -1
 
 def readData():
     try:
-        data = bus.read_byte(ADDRESS)
+        data = bus.read_byte(ADDRESS,0x00,32)
+        recived_message = ''.join(chr(b)for b in data if b != 0)
         return data
     except:
         print("Error cannot read data")
         return -1
 
 while True:
-    writeData()
+    message = input("Input message: ")
+    writeData(message)
     data = readData()
     print(data)
     time.sleep(1)
