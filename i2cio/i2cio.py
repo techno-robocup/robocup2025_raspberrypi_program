236c9665036816cca00a44436e91e371a3602c9e
k10k100 <keito.tottori@gmail.com>
2025-01-22 16:39:56 +0900

    Yahhoo!

--- i2cio/i2cio.py
+++ i2cio/i2cio.py
@@ -3,14 +3,26 @@ import smbus
 class readFailure(Exception):
   pass
 
+class writeFailure(Exception):
+  pass
+
 class i2cio:
   def __init__(self, address):
     self.bus = smbus.SMBus(1)
     target_address = 0x08
 
+  def writeData(self,data):
+    byte_data = [ord(c) for c in data]
+    cmd = [0x00]
+    try:
+      self.bus.write_i2c_block_data(ADDRESS,cmd,byte_data)
+    except:
+      raise writeFailure
+
   def readData(self):
     try:
       data = self.bus.read_byte(address)
+      recived_message = ''.join(chr(b)for b in data if b != 0)
     except:
       raise readFailure
     return data
--- main.py
+++ main.py
@@ -1,29 +1,13 @@
 import smbus #Python 3
 import time
+import i2cio.i2cio import i2cio
 
-bus = smbus.SMBus(1) #Pass
-
-ADDRESS = 0x08
-def writeData():
-    byte_data = [ord(c) for c in data]
-    try:
-        bus.write_i2c_block_data(ADDRESS,0x00,byte_data)
-    except:
-        print("Error cannot write data")
-        return -1
-
-def readData():
-    try:
-        data = bus.read_byte(ADDRESS,0x00,32)
-        recived_message = ''.join(chr(b)for b in data if b != 0)
-        return data
-    except:
-        print("Error cannot read data")
-        return -1
+if __name__ == "__main__":
+    i2c = i2cio(0x08)
 
 while True:
     message = input("Input message: ")
-    writeData(message)
-    data = readData()
+    i2cwriteData(message)
+    data = i2c.readData()
     print(data)
     time.sleep(1)
