f70c3018a5f08a93f42e681fe323c0aec4e1b2ca
k10k100 <keito.tottori@gmail.com>
2025-01-22 17:53:02 +0900

    kkkk

--- i2cio/i2cio.py
+++ i2cio/i2cio.py
@@ -12,8 +12,7 @@ class i2cio:
     self.bus = smbus.SMBus(1)
     self.target_address = address
 
-  def writeData(self, data -> [int]):
-    byte_data = [ord(c) for c in data]
+  def writeData(self, data:list[int]):
     cmd = [0x00]
     try:
       self.bus.write_i2c_block_data(self.target_address,cmd,byte_data)
@@ -21,6 +20,7 @@ class i2cio:
       raise writeFailure
 
   def readData(self):
+
     try:
       data = self.bus.read_byte(self.target_address)
       recived_message = ''.join(chr(b)for b in data if b != 0)
