9bc5de813fde0682a0fa5c728ddf47746a5f70c8
k10k100 <keito.tottori@gmail.com>
2025-01-27 22:43:56 +0900

    add reciveEvent

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ if __name__ == "__main__":
         send_data = list(message.split())
         send_data = [ int(i) for i in send_data ]
         i2c_device.writeData(send_data)
+
+        data = i2c_device.readData()
+        print(f"Recived data: {data}")
+
+        returnbits(data)
