d81e48673069a790a78c9dd026812a0043065cf9
rotarymars <s.goto2050@gmail.com>
2025-04-07 18:19:26 +0900

    Formatted codes
    #2

--- modules/camera.py
+++ modules/camera.py
@@ -2,33 +2,35 @@ from typing import Callable
 from picamera2 import Picamera2
 import settings
 
+
 class Camera:
 
     def __init__(self, PORT: int, controls: dict[str, any],
                  size: tuple[int, int], formats: str, lores_size: tuple[int,
                                                                         int],
                  pre_callback_func: Callable[[any], any]):
-      self.PORT = PORT
-      self.controls = controls
-      self.size = size
-      self.format = formats
-      self.lores_size = lores_size
-      self.pre_callback_func = pre_callback_func
-      self.cam = Picamera2(self.PORT)
-      self.cam.preview_configuration.main.size = self.size
-      self.cam.preview_configuration.main.format = self.format
-      self.cam.configure(
-          cam.create_preview_configuration(
-              main={
-                  "size": self.size,
-                "format": self.format },
-              lores={
-                  "size": self.lores_size,
-                  "format": self.format
-              },
-          ))
-      self.cam.pre_callback = self.pre_callback_func
-      self.cam.set_controls(self.controls)
+        self.PORT = PORT
+        self.controls = controls
+        self.size = size
+        self.format = formats
+        self.lores_size = lores_size
+        self.pre_callback_func = pre_callback_func
+        self.cam = Picamera2(self.PORT)
+        self.cam.preview_configuration.main.size = self.size
+        self.cam.preview_configuration.main.format = self.format
+        self.cam.configure(
+            cam.create_preview_configuration(
+                main={
+                    "size": self.size,
+                    "format": self.format
+                },
+                lores={
+                    "size": self.lores_size,
+                    "format": self.format
+                },
+            ))
+        self.cam.pre_callback = self.pre_callback_func
+        self.cam.set_controls(self.controls)
 
     def start_cam(self):
 
@@ -38,20 +40,19 @@ class Camera:
 
         self.cam.stop()
 
+
 Rescue_Camera = Camera(
-  PORT=settings.Rescue_Camera_PORT,
-  controls=settings.Rescue_Camera_Controls,
-  size=settings.Rescue_Camera_size,
-  formats=settings.Rescue_Camera_formats,
-  lores_size=settings.Rescue_Camera_lores_size,
-  pre_callback_func=settings.Rescue_Camera_Pre_Callback_func
-)
+    PORT=settings.Rescue_Camera_PORT,
+    controls=settings.Rescue_Camera_Controls,
+    size=settings.Rescue_Camera_size,
+    formats=settings.Rescue_Camera_formats,
+    lores_size=settings.Rescue_Camera_lores_size,
+    pre_callback_func=settings.Rescue_Camera_Pre_Callback_func)
 
 Linetrace_Camera = Camera(
-  PORT=settings.Linetrace_Camera_PORT,
-  controls=settings.Linetrace_Camera_Controls,
-  size=settings.Linetrace_Camera_size,
-  formats=settings.Linetrace_Camera_formats,
-  lores_size=settings.Linetrace_Camera_lores_size,
-  pre_callback_func=settings.Linetrace_Camera_Pre_Callback_func
-)
+    PORT=settings.Linetrace_Camera_PORT,
+    controls=settings.Linetrace_Camera_Controls,
+    size=settings.Linetrace_Camera_size,
+    formats=settings.Linetrace_Camera_formats,
+    lores_size=settings.Linetrace_Camera_lores_size,
+    pre_callback_func=settings.Linetrace_Camera_Pre_Callback_func)
--- modules/settings.py
+++ modules/settings.py
@@ -1,43 +1,46 @@
 from libcamera import controls
+
 DEBUG_MODE = False
 
+
 def Rescue_Camera_Pre_callback(request):
-  pass
+    pass
+
 
 def Linetrace_Camera_Pre_callback(request):
-  pass
+    pass
+
 
 Rescue_Camera_PORT = 1
 Rescue_Camera_Controls = {
-                           "AfMode": controls.AfModeEnum.Continuous,
-                           "AfSpeed": controls.AfSpeedEnum.Fast,
-                           "AeFlickerMode": controls.AeFlickerModeEnum.Manual,
-                           "AeFlickerPeriod": 10000,
-                           "AeMeteringMode":
-                           controls.AeMeteringModeEnum.Matrix,
-                           "AwbEnable": True,
-                           "AwbMode": controls.AwbModeEnum.Indoor,
-                           "HdrMode": controls.HdrModeEnum.Off
-                       }
+    "AfMode": controls.AfModeEnum.Continuous,
+    "AfSpeed": controls.AfSpeedEnum.Fast,
+    "AeFlickerMode": controls.AeFlickerModeEnum.Manual,
+    "AeFlickerPeriod": 10000,
+    "AeMeteringMode": controls.AeMeteringModeEnum.Matrix,
+    "AwbEnable": True,
+    "AwbMode": controls.AwbModeEnum.Indoor,
+    "HdrMode": controls.HdrModeEnum.Off
+}
 Rescue_Camera_size = (4608, 2592)
 Rescue_Camera_formats = "RGB888"
-Rescue_Camera_lores_size = (Rescue_Camera_size[0] // 4, Rescue_Camera_size[1] // 4)
+Rescue_Camera_lores_size = (Rescue_Camera_size[0] // 4,
+                            Rescue_Camera_size[1] // 4)
 Rescue_Camera_Pre_Callback_func = Rescue_Camera_Pre_callback
 
 Linetrace_Camera_PORT = 0
 Linetrace_Camera_Controls = {
-                             "AfMode": controls.AfModeEnum.Manual,
-                             "LensPosition": 1.0 / 0.03,
-                             "AeFlickerMode":
-                             controls.AeFlickerModeEnum.Manual,
-                             "AeFlickerPeriod": 10000,
-                             "AeMeteringMode":
-                             controls.AeMeteringModeEnum.Matrix,
-                             "AwbEnable": False,
-                             "AwbMode": controls.AwbModeEnum.Indoor,
-                             "HdrMode": controls.HdrModeEnum.Night
-                         }
+    "AfMode": controls.AfModeEnum.Manual,
+    "LensPosition": 1.0 / 0.03,
+    "AeFlickerMode": controls.AeFlickerModeEnum.Manual,
+    "AeFlickerPeriod": 10000,
+    "AeMeteringMode": controls.AeMeteringModeEnum.Matrix,
+    "AwbEnable": False,
+    "AwbMode": controls.AwbModeEnum.Indoor,
+    "HdrMode": controls.HdrModeEnum.Night
+}
 Linetrace_Camera_size = (4608, 2592)
 Linetrace_Camera_formats = "RGB888"
-Linetrace_Camera_lores_size = (Linetrace_Camera_size[0] // 4, Linetrace_Camera_size[1] // 4)
+Linetrace_Camera_lores_size = (Linetrace_Camera_size[0] // 4,
+                               Linetrace_Camera_size[1] // 4)
 Linetrace_Camera_Pre_Callback_func = Linetrace_Camera_Pre_callback
