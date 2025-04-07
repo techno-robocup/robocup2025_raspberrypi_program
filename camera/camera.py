532d58b7f9b1b479cd1947c227d051f1b6ffe09f
rotarymars <s.goto2050@gmail.com>
2025-04-07 17:58:13 +0900

    Finished implementing camera class (Commit for Issue #2)
    Added configs for both linetrace and rescue camera

--- camera/camera.py
+++ camera/camera.py
@@ -66,3 +66,32 @@ class Camera:
     """
 
         cam.stop()
+
+from libcamera import controls
+
+Rescue_Camera_Size = (4608, 2592)
+Linetrace_Camera_Size = (4608, 2592)
+
+def Rescue_Camera_Pre_callback(request):
+  pass
+
+def Linetrace_Camera_Pre_callback(request):
+  pass
+
+Rescue_Camera = Camera(
+  PORT = 1,
+  controls = { "AfMode": controls.AfModeEnum.Continuous, "AfSpeed": controls.AfSpeedEnum.Fast, "AeFlickerMode": controls.AeFlickerModeEnum.Manual, "AeFlickerPeriod": 10000, "AeMeteringMode": controls.AeMeteringModeEnum.Matrix, "AwbEnable": True, "AwbMode": controls.AwbModeEnum.Indoor, "HdrMode": controls.HdrModeEnum.Off },
+  size = Rescue_Camera_Size,
+  lores_size = (Rescue_Camera_Size[0]//4, Rescue_Camera_Size[1]//4),
+  formats = "RGB888",
+  pre_callback_func = Rescue_Camera_Pre_callback
+)
+
+Linetrace_Camera = Camera(
+  PORT = 0,
+  controls = { "AfMode": controls.AfModeEnum.Manual, "LensPosition": 1.0 / 0.03, "AeFlickerMode": controls.AeFlickerModeEnum.Manual, "AeFlickerPeriod": 10000, "AeMeteringMode": controls.AeMeteringModeEnum.Matrix, "AwbEnable": False, "AwbMode": controls.AwbModeEnum.Indoor, "HdrMode": controls.HdrModeEnum.Night },
+  size = Linetrace_Camera_Size,
+  lores_size = (Linetrace_Camera_Size[0]//4, Linetrace_Camera_Size[1]//4),
+  formats = "RGB888",
+  pre_callback_func = Linetrace_Camera_Pre_callback
+)
