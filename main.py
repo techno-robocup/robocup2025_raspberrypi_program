1d486f5cdc48fe51cc2abb0acb2c5343ac85f716
rotarymars <s.goto2050@gmail.com>
2025-04-14 17:24:46 +0900

    main.py: Changed to support modules changes

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import time
 import random
-from modules.i2cio import i2cio
-from modules.camera import Rescue_Camera, Linetrace_Camera
+import modules.i2cio
+import modules.camera
+import modules.settings
 from time import sleep
+
+
+Rescue_Camera = modules.camera.Camera(
+    PORT=modules.settings.Rescue_Camera_PORT,
+    controls=modules.settings.Rescue_Camera_Controls,
+    size=modules.settings.Rescue_Camera_size,
+    formats=modules.settings.Rescue_Camera_formats,
+    lores_size=modules.settings.Rescue_Camera_lores_size,
+    pre_callback_func=modules.settings.Rescue_Camera_Pre_Callback_func)
+
+Linetrace_Camera = Camera(
+    PORT=modules.settings.Linetrace_Camera_PORT,
+    controls=modules.settings.Linetrace_Camera_Controls,
+    size=modules.settings.Linetrace_Camera_size,
+    formats=modules.settings.Linetrace_Camera_formats,
+    lores_size=modules.settings.Linetrace_Camera_lores_size,
+    pre_callback_func=modules.settings.Linetrace_Camera_Pre_Callback_func)
 
 if __name__ == "__main__":
     Linetrace_Camera.start_cam()
