a04963dba3757dd68ddfb49cb68bdad9bd05ba87
rotarymars <s.goto2050@gmail.com>
2025-05-09 18:19:10 +0900

    Add: Added debug images on git

---
+++ i2ctest/modules/i2cio.py
@@ -0,0 +1,33 @@
+import smbus
+
+
+class readFailure(Exception):
+  pass
+
+
+class writeFailure(Exception):
+  pass
+
+
+class i2cio:
+
+  def __init__(self, address):
+    self.bus = smbus.SMBus(1)
+    self.target_address = address
+
+  def writeData(self, data: list[int]):
+    cmd = 0x00
+    #(address(int), cmd(int), data(list[int]))
+    try:
+      self.bus.write_i2c_block_data(self.target_address, cmd, data)
+    except Exception as e:
+      print("failed", e)
+
+  def readData(self):
+    data = 0x00
+    try:
+      data = self.bus.read_byte(self.target_address)
+    except Exception as e:
+      print("failed", e)
+      return None
+    return data
