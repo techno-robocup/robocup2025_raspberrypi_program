b10ebd63a94a7970e53d23f6e7116a6a51777010
rotarymars <s.goto2050@gmail.com>
2025-06-06 17:09:57 +0900

    removed test func

--- main.py
+++ main.py
@@ -19,29 +19,6 @@ import modules.uart
 #     pre_callback_func=modules.settings.Linetrace_Camera_Pre_Callback_func)
 
 
-def serial_read():
-    available_ports = list(list_ports.comports())
-    
-    if not available_ports:
-        print("No serial ports found!")
-        return
-        
-    print("Available serial ports:")
-    for port in available_ports:
-        print(f"Port: {port.device}, Description: {port.description}")
-
-    Serial_Port_id = available_ports[0].device
-    print(f"Using port: {Serial_Port_id}")
-    
-    Serial_Port = serial.Serial(Serial_Port_id, 9600, timeout=3)
-    while True:
-        if Serial_Port != '':
-            data = Serial_Port.readline()
-            if data:
-                hex_data = ' '.join([f'{b:02X}' for b in data])
-                print(f"Hex: {hex_data}")
-
-
 if __name__ == "__main__":
   # Linetrace_Camera.start_cam()
   serial_read()
