e21f938634b530e79293897ab8a172fef19ad5ad
rotarymars <s.goto2050@gmail.com>
2025-06-06 16:23:41 +0900

    commented out program

--- main.py
+++ main.py
@@ -2,37 +2,50 @@ import serial
 import modules.camera
 import modules.settings
 import modules.i2cio
+from serial.tools import list_ports
 
-Rescue_Camera = modules.camera.Camera(
-    PORT=modules.settings.Rescue_Camera_PORT,
-    controls=modules.settings.Rescue_Camera_Controls,
-    size=modules.settings.Rescue_Camera_size,
-    formats=modules.settings.Rescue_Camera_formats,
-    lores_size=modules.settings.Rescue_Camera_lores_size,
-    pre_callback_func=modules.settings.Rescue_Camera_Pre_Callback_func)
+# Rescue_Camera = modules.camera.Camera(
+#     PORT=modules.settings.Rescue_Camera_PORT,
+#     controls=modules.settings.Rescue_Camera_Controls,
+#     size=modules.settings.Rescue_Camera_size,
+#     formats=modules.settings.Rescue_Camera_formats,
+#     lores_size=modules.settings.Rescue_Camera_lores_size,
+#     pre_callback_func=modules.settings.Rescue_Camera_Pre_Callback_func)
 
-Linetrace_Camera = modules.camera.Camera(
-    PORT=modules.settings.Linetrace_Camera_PORT,
-    controls=modules.settings.Linetrace_Camera_Controls,
-    size=modules.settings.Linetrace_Camera_size,
-    formats=modules.settings.Linetrace_Camera_formats,
-    lores_size=modules.settings.Linetrace_Camera_lores_size,
-    pre_callback_func=modules.settings.Linetrace_Camera_Pre_Callback_func)
+# Linetrace_Camera = modules.camera.Camera(
+#     PORT=modules.settings.Linetrace_Camera_PORT,
+#     controls=modules.settings.Linetrace_Camera_Controls,
+#     size=modules.settings.Linetrace_Camera_size,
+#     formats=modules.settings.Linetrace_Camera_formats,
+#     lores_size=modules.settings.Linetrace_Camera_lores_size,
+#     pre_callback_func=modules.settings.Linetrace_Camera_Pre_Callback_func)
 
 
 def serial_read():
-  Serial_Port_id = "/dev/ttyUSB0"
-  Serial_Port = serial.Serial(Serial_Port_id, 9600, timeout=3)
-  while True:
-    if Serial_Port != '':
-      data = Serial_Port.readline()
-      if data:  # Only process if we received data
-        hex_data = ' '.join([f'{b:02X}' for b in data])
-        print(f"Hex: {hex_data}")
+    available_ports = list(list_ports.comports())
+    
+    if not available_ports:
+        print("No serial ports found!")
+        return
+        
+    print("Available serial ports:")
+    for port in available_ports:
+        print(f"Port: {port.device}, Description: {port.description}")
+
+    Serial_Port_id = available_ports[0].device
+    print(f"Using port: {Serial_Port_id}")
+    
+    Serial_Port = serial.Serial(Serial_Port_id, 9600, timeout=3)
+    while True:
+        if Serial_Port != '':
+            data = Serial_Port.readline()
+            if data:
+                hex_data = ' '.join([f'{b:02X}' for b in data])
+                print(f"Hex: {hex_data}")
 
 
 if __name__ == "__main__":
-  Linetrace_Camera.start_cam()
+  # Linetrace_Camera.start_cam()
   while True:
     pass
-  Linetrace_Camera.stop_cam()
+  # Linetrace_Camera.stop_cam()
