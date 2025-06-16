5fbf554d11c857e3a7207151252913fa8944351a
rotarymars <s.goto2050@gmail.com>
2025-06-16 17:32:29 +0900

    created slope calculating logic and functions

--- main.py
+++ main.py
@@ -35,6 +35,12 @@ def send_speed(l : int, r : int):
   return
 logger.info("OBJECTS INITIALIZED")
 
+def fix_to_range(x : int, min_num : int, max_num : int):
+  return max(min_num, min(x, max_num))
+
+def compute_moving_value(current_slope : int):
+  return math.sqrt(1 / current_slope)
+
 message_id = 0
 if __name__ == "__main__":
   try:
@@ -42,7 +48,14 @@ if __name__ == "__main__":
       uart_io.send_message(Message(message_id, "GET button"))
       message = uart_io.receive_message()
       if message.getMessage() == "OFF":
+        logger.debug("BUTTON OFF")
         continue
+      logger.debug("BUTTON ON")
+      current_slope = modules.settings.lastblackline
+      logger.debug(f"CURRENT SLOPE: {current_slope}")
+      moving_value = compute_moving_value(current_slope)
+      logger.debug(f"MOVING VALUE: {moving_value}")
+      send_speed(fix_to_range(1500 + moving_value, 1000, 2000), fix_to_range(1500 - moving_value, 1000, 2000))
       message_id += 1
   except KeyboardInterrupt:
     logger.info(f"STOPPING PROCESS BY KeyboardInterrupt at line {sys.exc_info()[2].tb_lineno}")
