789040594b295128b063533c278c7022aa036641
rotarymars <s.goto2050@gmail.com>
2025-07-07 17:58:25 +0900

    Revert "temp change 2"
    This reverts commit 070ed8ba79dff1659f7e5d2c29fc7f1e4a0172ae.

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
         # send_speed(fix_to_range(1500 + moving_value, 1000, 2000),
         #            fix_to_range(1500 - moving_value, 1000, 2000))
         send_speed(2000, 2000)
-        print(f"MESSAGE: {uart_io.receive_message()}")
+        print(f"MESSAGE: {message.getMessage()}")
       message_id += 1
   except KeyboardInterrupt:
     logger.info(
