920eda801dc83ceb9fdb46d231e385fac32f0535
rotarymars <s.goto2050@gmail.com>
2025-09-01 16:40:47 +0900

    Added debug message for main.py on red stop

--- main.py
+++ main.py
@@ -162,6 +162,7 @@ def main_loop():
     else:
       if modules.settings.stop_requested:
         send_speed(1500, 1500)
+        logger.debug("Red stop")
         return
       if modules.settings.slope is None:
         send_speed(default_speed - 10, default_speed - 10)
